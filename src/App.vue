1cc4344f33e1f7eddffc20496ac688b5b024f85c
Jacky <jackyday2007@gmail.com>
2024-07-16 14:49:28 +0800

    merge two vue

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,9 @@
 </script>
   
 <style>
-<<<<<<< HEAD
+
   
   body {
     padding-top: 70px; /* 70px 是 Bootstrap 默认导航栏的高度 */
   }
-
-
-=======
-  body{
-    padding-top: 70px;
-  }
->>>>>>> f84162522616150573df3c2dd2235e7b0408374f
 </style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,21 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router"
 import Home from "@/views/Home.vue"
 import NotFound from "@/views/NotFound.vue"
-<<<<<<< HEAD
+import Problems from "@/views/problems/router-problems"
 import routerOrders from '@/views/orders/router-orders'
-
-=======
-import Problems from "@/views/problems/ProblemList.vue"
->>>>>>> f84162522616150573df3c2dd2235e7b0408374f
-
 const routes = [
     { name: "home-link", path: "/", component: Home },
     { name: "notfound-link", path: "/:pathMatch(.*)*", component: NotFound },
-<<<<<<< HEAD
     ...routerOrders,
-=======
-    { name: "router-problems-link", path: "/router/problems", component: Problems },
->>>>>>> f84162522616150573df3c2dd2235e7b0408374f
+    ...Problems,
 ]
 
 
