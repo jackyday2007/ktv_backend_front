10e3f9d0de8444bc6d6c36e5ffeb654caca8b8ed
Jacky <jackyday2007@gmail.com>
2024-07-17 15:08:34 +0800

    select final

---
+++ src/components/order/OrdersRows.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <select class="form-select" id="inputGroupSelect01" :value="modelValue" @input="dochange">
+        <option v-for=" option in options " :key="option" :value="option">{{ option }}</option>
+    </select>
+
+
+
+        
+</template>
+    
+<script setup>
+
+    const props = defineProps(["total", "options", "modelValue"]);
+    const emits = defineEmits(["update:modelValue","change"])
+
+    function dochange(event) {
+        emits('update:modelValue', event.target.value);
+        emits("change");
+    }
+
+
+    
+</script>
+    
+<style>
+    
+</style>
--- src/views/orders/Orders.vue
+++ src/views/orders/Orders.vue
@@ -3,18 +3,28 @@
     <h3>訂位清單</h3>
     <div class="input-group mb-3">
         <span class="input-group-text" id="inputGroup-sizing-default">訂單編號</span>
-        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        <input v-model="orderId" @input="orderList(0)" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
         <span class="input-group-text" id="inputGroup-sizing-default">會員編號</span>
         <input v-model="memberId" @input="orderList(0)"  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
         <span class="input-group-text" id="inputGroup-sizing-default">包廂號</span>
-        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-        <label class="input-group-text" for="inputGroupSelect01">銷費狀態</label>
-        <select class="form-select" id="inputGroupSelect01">
-            <option selected>Choose...</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+        <input v-model="roomId" @input="orderList(0)" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        <span class="input-group-text" id="inputGroup-sizing-default">消費狀態</span>
+        <!-- <input v-model="status" @input="orderList(0)" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"> -->
+        <select v-model="status" @change="orderList(0)" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+            <option>預約</option>
+            <option>等待</option>
+            <option>入場</option>
+            <option>結帳</option>
+            <option>離場</option>
         </select>
+        <label class="input-group-text" for="inputGroupSelect01">顯視筆數</label>
+        <OrdersRows
+            :total="total"
+            :options="[2, 3, 4, 5, 10]"
+            v-model="rows"
+            @change="orderList">
+        >
+        </OrdersRows>
     </div>
 
     <table class="table table-striped table-hover">
@@ -45,7 +55,7 @@
                     離場時間
                 </th>
                 <th>
-                    目前銷費狀態
+                    銷費狀態
                 </th>
             </tr>
         </thead>
@@ -61,7 +71,7 @@
 
     </div>
 
-    <!-- <button @click="orderList" >按鈕</button> -->
+
 
 </template>
     
@@ -70,6 +80,7 @@
     import OrdersList from '@/components/order/OrdersList.vue';
     import { onMounted, ref } from 'vue';
     import Swal from 'sweetalert2';
+    import OrdersRows from '@/components/order/OrdersRows.vue'
 
     onMounted (
         function() {
@@ -77,6 +88,8 @@
         }
     )
 
+
+    // 分頁
     const total = ref(0);
     const pages = ref(0);
     const orders = ref([ ]);
@@ -84,11 +97,25 @@
     const rows = ref(4);
     const lastPageRows = ref(0);
     const start = ref(0);
+
+    // 搜尋條件
     const memberId = ref("");
+    const orderId = ref("");
+    const roomId = ref("");
+    const status = ref("");
+
 
     function orderList(page) {
         if (memberId.value === "") {
             memberId.value = null;
+        }
+
+        if (orderId.value === "") {
+            orderId.value = null;
+        }
+
+        if ( status.value === "" ) {
+            status.value = null;
         }
 
         //分頁計算
@@ -105,7 +132,10 @@
             "max": rows.value,
             "dir" : false,
             "order" : "orderId",
-            "memberId" : memberId.value
+            "memberId" : memberId.value,
+            "roomId" : roomId.value,
+            "orderId" : orderId.value,
+            "status" : status.value,
         }
 
         axiosapi.post( "/ktv-app/ktvbackend/orders/findTest", request)
