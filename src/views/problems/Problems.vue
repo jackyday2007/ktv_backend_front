08f42dfcfe17b3fe7f445949aac4fa9a3e28c29a
Jeff <xz850412@gmail.com>
2024-07-19 14:19:23 +0800

    final problem

--- src/views/problems/Problems.vue
+++ src/views/problems/Problems.vue
@@ -9,10 +9,9 @@
         <span class="input-group-text" id="inputGroup-sizing-default">包廂狀態</span>
         <select v-model="searchStatus" @change="findByStatus(searchStatus)" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
             <option></option>
-            <option>未使用</option>
-            <option>使用</option>
-            <option>維修</option>
-            <option>停用</option>
+            <option>處理中</option>
+            <option>結案</option>
+
         </select>
         
   </div>
@@ -20,7 +19,7 @@
   
      <ProblemModal ref="problemRef":is-show-insert-button="isShowInsertButton" v-model="problem" @insert="callCreate" @update="callModify"></ProblemModal>
      
-
+     
   <table class="table table-striped table-hover">
       <thead>
         <tr>
@@ -38,10 +37,24 @@
       <ProblemList v-for="item in problems"
         :key="item.problemId"
         :problem="item"
-        @problem-update="callModify"></ProblemList>
+        @dblclick="openModal('update', item.problemId)"
+        @problem-update="openModal('update', item.problemId)"></ProblemList>
     </tbody>
     
   </table>
+  <div style="display:flex; justify-content: center;" >
+        <div  v-show="total!=0">
+            <Paginate :first-last-button="true" 
+                first-button-text="&lt;&lt;" 
+                last-button-text="&gt;&gt;"
+                prev-text="&lt;" next-text="&gt;"
+                :page-count="pages"
+                :initial-page="current"
+                v-model="current"
+                :click-handler="callFind">
+            </Paginate>
+        </div>
+    </div>
 </template>
   
 <script setup >
@@ -50,12 +63,13 @@
   import { ref, onMounted } from 'vue';
   import axiosapi from '@/plugins/axios';
   import Swal from 'sweetalert2';
+  import Paginate from 'vuejs-paginate-next'; 
   
 
    //分頁 Start
     const total = ref(0);   //總資料筆數
     const pages = ref(0);   //總共頁數
-    const current = ref(0); //目前頁碼
+    const current = ref(1); //目前頁碼
     const rows = ref(4);    //最多抓幾筆資料
     const start = ref(0);   //從哪裡開始抓資料
     const lastPageRows = ref(0); 
@@ -69,53 +83,13 @@
     const searchProblemId = ref("");
     const searchRoomId = ref("");
     const searchStatus = ref("");
+
+
     onMounted (
         function() {
           callFind()
         }
     )
-
-    function callFind(page){
-
-      console.log("page" , page);
-
-      if(page){
-        start.value = ( page -1 ) * rows.value;
-        current.value = page;
-    }else{
-        start.value = 0;
-        current.value = 1;
-    }
-    if(findName.value==""){
-        findName.value = null;
-    }
-
-    let request = {
-        "start":start.value,
-        "max":rows.value,
-        "dir":false,
-        "order":"room",
-        // "name":findName.value
-    }
-
-    axiosapi.post('/ktv-app/problems/findAll', request).then(function(response){
-          console.log('response=', response);
-          problems.value = response.data.list;
-          total.value = response.data.count;
-          pages.value = Math.ceil(total.value / rows.value);
-          lastPageRows.value = total.value % rows.value;
-
-          setTimeout(function() {
-                  Swal.close();
-              }, 500); 
-        }).catch(function(error){
-            console.log('error=', error);
-            Swal.fire({
-              icon: 'error',
-              text: '查詢失敗: ' + error.message,
-            });
-        });
-    }
 
     function openModal(action , problemId){
         if(action==='insert'){
@@ -124,8 +98,108 @@
         }else if(action==='update'){
             isShowInsertButton.value = false;
             callFindById(problemId);
+            // callModify();
         }
         problemRef.value.showModal();
+    }
+
+    function callFindById(problemId){
+      axiosapi.get(`/ktv-app/problems/findByProblemId/${problemId}`).then(function(response){
+        
+        problem.value = response.data.list[0];
+        problemRef.value.showModal();
+        // callModify();
+      }).catch(function(error){
+            Swal.fire({
+          icon: 'error',
+          text: '查詢失敗: ' + error.message,
+        });
+      });
+    }
+
+    function callModify() {
+      
+        Swal.fire({
+            text: "執行中......",
+            allowOutsideClick: false,
+            showConfirmButton: false,
+        });
+
+        // 處理空值
+        for (let key in problem.value) {
+          if (problem.value[key] === "") {
+            problem.value[key] = null;
+          }
+        }
+
+        console.log("problem----",problem.value);
+        
+        axiosapi.put(`/ktv-app/problems/modify/${problem.value.problemId}` , problem.value).then(function(response){
+          console.log("response", response);
+          if(response.data.success)  {
+                Swal.fire({
+                    icon: "success",
+                    text: response.data.message,
+                }).then(function() {
+                  problemRef.value.hideModal();
+                    callFind(current.value);
+                });
+            } else {
+                Swal.fire({
+                    icon: "warning",
+                    text: response.data.message,
+                });
+            }
+        }).catch(function(error) {
+            console.log("error", error);
+            Swal.fire({
+                icon: "error",
+                text: "修改錯誤："+error.message,
+            });
+        });
+
+    }
+
+    function callFind(page){
+
+    if(page){
+      start.value = ( page -1 ) * rows.value;
+      current.value = page;
+    }else{
+      start.value = 0;
+      current.value = 1;
+    }
+    if(findName.value==""){
+      findName.value = null;
+    }
+
+    let request = {
+      "start":start.value,
+      "max":rows.value,
+      "dir":false,
+      // "order":"room",
+      // "name":findName.value
+    }
+
+    axiosapi.post('/ktv-app/problems/findAll', request).then(function(response){
+        problems.value = response.data.list;
+        total.value = response.data.count;
+        console.log("total.value=",total.value);
+        pages.value = Math.ceil(total.value / rows.value);
+        console.log("pages.value=",pages.value);
+        lastPageRows.value = total.value % rows.value;
+        console.log("lastPageRows.value=",lastPageRows.value);
+
+        setTimeout(function() {
+                Swal.close();
+            }, 500); 
+      }).catch(function(error){
+          console.log('error=', error);
+          Swal.fire({
+            icon: 'error',
+            text: '查詢失敗: ' + error.message,
+          });
+      });
     }
 
     function callCreate() {
@@ -134,28 +208,13 @@
             allowOutsideClick: false,
             showConfirmButton: false,
         });
+    // 處理空值
+    for (let key in problem.value) {
+        if (problem.value[key] === "") {
+          problem.value[key] = null;
+        }
+      }
 
-        if(problem.value.problemId=="") {
-          problem.value.problemId = null;
-        }
-        if(problem.value.eventCase=="") {
-          problem.value.eventCase = null;
-        }
-        if(problem.value.room=="") {
-          problem.value.room = null;
-        }
-        if(problem.value.content=="") {
-          problem.value.content = null;
-        }
-        if(problem.value.eventDate=="") {
-          problem.value.eventDate = null;
-        }
-        if(problem.value.closeDate=="") {
-          problem.value.closeDate = null;
-        }
-        if(problem.value.status=="") {
-          problem.value.status = null;
-        }
       axiosapi.post("/ktv-app/problems/create", problem.value)
         .then(function(response) {
           if(response.data.success)  {
@@ -246,72 +305,6 @@
       
     }
 
-    function callFindById(problemId){
-      axiosapi.get(`/ktv-app/problems/${problemId}`).then(function(response){
-        problem.value = response.data;
-        problemRef.value.showModal();
-      }).catch(function(error){
-            Swal.fire({
-          icon: 'error',
-          text: '查詢失敗: ' + error.message,
-        });
-      });
-    }
-
-    function callModify() {
-        Swal.fire({
-            text: "執行中......",
-            allowOutsideClick: false,
-            showConfirmButton: false,
-        });
-
-        if(problem.value.problemId=="") {
-          problem.value.problemId = null;
-        }
-        if(problem.value.eventCase=="") {
-          problem.value.eventCase = null;
-        }
-        if(problem.value.room=="") {
-          problem.value.room = null;
-        }
-        if(problem.value.content=="") {
-          problem.value.content = null;
-        }
-        if(problem.value.eventDate=="") {
-          problem.value.eventDate = null;
-        }
-        if(problem.value.closeDate=="") {
-          problem.value.closeDate = null;
-        }
-        if(problem.value.status=="") {
-          problem.value.status = null;
-        }
-        
-        axiosapi.put(`/ktv-app/problems/modify/${problem.value.problemId}` , problem.value).then(function(response){
-          
-          if(response.data.success)  {
-                Swal.fire({
-                    icon: "success",
-                    text: response.data.message,
-                }).then(function() {
-                  problemRef.value.hideModal();
-                    callFind(current.value);
-                });
-            } else {
-                Swal.fire({
-                    icon: "warning",
-                    text: response.data.message,
-                });
-            }
-        }).catch(function(error) {
-            console.log("error", error);
-            Swal.fire({
-                icon: "error",
-                text: "修改錯誤："+error.message,
-            });
-        });
-
-    }
 </script>
   
 <style>
