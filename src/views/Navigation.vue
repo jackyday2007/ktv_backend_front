aa059515b76642c9dd6ab71b4553cf4f544feee3
jackyday2007 <jackyday2007@gmail.com>
2024-07-24 01:13:19 +0800

    add images

--- src/views/Navigation.vue
+++ src/views/Navigation.vue
@@ -3,11 +3,15 @@
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
+                
+
             </button>
+            
             
             <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
                 <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+                    <img src="../assets/GOLD-PANDA.png" style="width: 100px; height: 40px;">
+                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">KTV櫃台系統</h5>
                     <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                     <div class="offcanvas-body">
